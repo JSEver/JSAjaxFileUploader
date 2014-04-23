68d7706d046eb19f9a744a24e34ef35fa70318ad
mujahid095ads <mujahid095ads@gmail.com>
2014-04-23 16:32:35 +0530

    Update JQuery.JSAjaxFileUploader.js

--- JQuery.JSAjaxFileUploader.js
+++ JQuery.JSAjaxFileUploader.js
@@ -1,5 +1,5 @@
 /*The MIT License (MIT)
-Copyright (c) 2014 https://github.com/
+Copyright (c) 2014 https://github.com/JSEver/JSAjaxFileUploader/
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
